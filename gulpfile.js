b8054cbfe72c3d22724cf5dd5dac62cb82cdcd77
Ryo Takei <shtickler11@gmail.com>
2019-11-05 17:43:39 -0800

    catch up ... added mixins variables btn stylesJS watchJS and modified existing files

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,3 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var cssvars = require('postcss-simple-vars');
-var nested = require('postcss-nested');
-var cssImport = require('postcss-import');
-
-gulp.task('default', function() {
-  console.log("hooray you created a gulp task.");
-});
-
-
-gulp.task('html', function() {
-  console.log("you are running the html task; something useful is happening");
-});
-
-gulp.task('styles', function() {
-  return gulp.src('./app/assets/styles/styles.css') // return bc asynchronous
-    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-    .pipe(gulp.dest('./app/temp/styles'));
-});
-
-
-gulp.task('watch', function(){
-
-  watch('./app/index.html', function(){
-    gulp.start('html');
-  });
-
-  watch('./app/assets/styles/**/*.css', function(){
-    gulp.start('styles');
-  });
-
-});
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
